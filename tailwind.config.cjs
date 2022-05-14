3777a12889be2158db7352ca4d677f97aa0cff6e
J.B.C <juanbicortelezzi@gmail.com>
2022-05-13 22:07:37 -0300

    feat: add daisyui and tailwind fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,5 +3,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3ABFF8",
+          secondary: "#828DF8",
+          accent: "#F471B5",
+          neutral: "#1D283A",
+          "base-100": "#0F1729",
+          info: "#0CA6E9",
+          success: "#2BD4BD",
+          warning: "#F4C152",
+          error: "#FB6F84",
+        },
+      },
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
